c9658daba9f57a3737c2a86f8bca0d0d7c5f271f
Aman <ar3698892@gmail.com>
2026-05-25 16:37:23 +0530

    Assign Role to user

---
+++ controllers/user/assignRoleToUser.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const userSchema = require('../../models/tenant/userSchema');
+const getTenantModel = require('../../utils/getTenantModel');
+const createHttpError = require('http-errors');
+const { STATUS_CODE, ERROR_MESSAGE } = require('../../utils/constant');
+const redis = require('../../services/cache');
+
+
+module.exports = async function (userId, roleId, dbName) {
+    const User = getTenantModel(dbName, 'User', userSchema);
+
+    const user = await User.findByIdAndUpdate(userId, { role: roleId }, { new: true });
+    if (!user) throw new createHttpError(STATUS_CODE.NOT_FOUND, ERROR_MESSAGE.USER_NOT_FOUND);
+
+    await redis.del(`user:${dbName}`);
+    return user;
+}
