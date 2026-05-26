1836bbcd9987a6146a7dfa524b1e95a597e13824
Aman <ar3698892@gmail.com>
2026-05-26 10:37:31 +0530

    Handle document duplication naming

--- controllers/document/getPresignedUploadUrl.js
+++ controllers/document/getPresignedUploadUrl.js
@@ -6,8 +6,11 @@ const path = require('node:path');
 const redis = require('../../services/cache');
 
 module.exports = async function (tenant, fileData, userId) {
-    const { fileName, contentType, folderId = null, size } = fileData;
+    let { fileName, contentType, folderId = null, size } = fileData;
     const { slug: tenantSlug, dbName, _id: tenantId } = tenant;
+
+    const ext = path.extname(fileName);
+    const baseName = path.basename(fileName, ext);
 
     const key = generateS3Key(tenantSlug, fileName);
     const url = await generateUploadUrl(key, contentType);
@@ -15,6 +18,26 @@ module.exports = async function (tenant, fileData, userId) {
     const storedName = path.basename(key);
 
     const Document = getTenantModel(dbName, 'Document', documentSchema);
+
+    const existingDocs = await Document.find({
+        originalFileName: new RegExp(`^${baseName}(_\\d+)?${ext}$`, 'i')
+    }).sort({ createdAt: -1 });
+
+    if (existingDocs.length > 0) {
+        let maxVersion = 0;
+
+        existingDocs.forEach(doc => {
+            const match = doc.originalFileName.match(/_(\d+)\.[^.]+$/);
+
+            if (match) {
+                maxVersion = Math.max(maxVersion, parseInt(match[1], 10));
+            } else {
+                maxVersion = Math.max(maxVersion, 1);
+            }
+        });
+
+        fileName = `${baseName}_${maxVersion + 1}${ext}`;
+    }
 
     const document = await Document.create({
         tenantId,
