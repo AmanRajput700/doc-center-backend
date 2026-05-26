43acaad9a4fdd0a435b0af3012d262958af929fa
Aman <ar3698892@gmail.com>
2026-05-26 12:57:17 +0530

    Added pagination

--- controllers/user/getUserByTenant.js
+++ controllers/user/getUserByTenant.js
@@ -3,12 +3,25 @@ const redis = require('../../services/cache');
 const userSchema = require('../../models/tenant/userSchema');
 const roleSchema = require('../../models/tenant/roleSchema');
 
-module.exports = async function (dbName) {
+module.exports = async function (page, limit, dbName) {
+    const skip = (page - 1) * limit;
     const User = getTenantModel(dbName, 'User', userSchema);
     const Role = getTenantModel(dbName, 'Role', roleSchema);
-    const users = await redis.get(`user:${dbName}`);
-    if (users) return JSON.parse(users);
-    const dbUsers = await User.find({}, { _id: 1, firstName: 1, lastName: 1, email: 1, role: 1, status: 1, lastLogin: 1, lastActivateAt: 1 }).populate('role', 'name').lean();
-    await redis.set(`user:${dbName}`, JSON.stringify(dbUsers), "EX", 300);
-    return dbUsers;
+    const totalUsers = await User.countDocuments();
+    const users = await User.find({}, { _id: 1, firstName: 1, lastName: 1, email: 1, role: 1, status: 1, lastLogin: 1, lastActivateAt: 1 })
+        .populate('role', 'name')
+        .skip(skip)
+        .limit(limit)
+        .lean();
+
+    const totalPages = Math.ceil(totalUsers / limit);
+    const paginationData = {
+        totalUsers,
+        totalPages,
+        currentPage: page,
+        pageSize: limit,
+        hasNextPage: page < totalPages,
+        hasPreviousPage: page > 1
+    }
+    return { paginationData, users };
 }
