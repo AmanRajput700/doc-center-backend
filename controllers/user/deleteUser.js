f40d2a556c1de0107669628bec1cbab7be4ad6b8
Aman <ar3698892@gmail.com>
2026-05-26 11:17:47 +0530

    delete user api

---
+++ controllers/user/deleteUser.js
@@ -0,0 +1,19 @@
+const userSchema = require('../../models/tenant/userSchema');
+const { STATUS_CODE, ERROR_MESSAGE } = require('../../utils/constant');
+const createHttpError = require('http-errors');
+const getTenantModel = require('../../utils/getTenantModel');
+const redis = require('../../services/cache');
+const TenantUserMap = require('../../models/root/TenantUserMap');
+const inviteMemberSchema = require('../../models/tenant/inviteMemberSchema');
+
+module.exports = async function (userId, dbName) {
+    const User = getTenantModel(dbName, 'User', userSchema);
+    const InviteMember = getTenantModel(dbName, 'InviteMember', inviteMemberSchema);
+
+    const deletedUser = await User.findOneAndDelete({ _id: userId });
+    await TenantUserMap.deleteOne({ email: deletedUser.email });
+    await InviteMember.deleteOne({ email: deletedUser.email });
+    if (!deletedUser) throw new createHttpError(STATUS_CODE.NOT_FOUND, ERROR_MESSAGE.USER_NOT_FOUND);
+    await redis.del(`user:${dbName}`);
+    return deletedUser;
+}
