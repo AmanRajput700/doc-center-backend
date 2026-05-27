4f133f4edeb2fa74fb301eb59c9df5a0230573be
Aman <ar3698892@gmail.com>
2026-05-27 16:45:51 +0530

    Implemented search for the user module

--- controllers/user/getUserByTenant.js
+++ controllers/user/getUserByTenant.js
@@ -3,12 +3,41 @@ const redis = require('../../services/cache');
 const userSchema = require('../../models/tenant/userSchema');
 const roleSchema = require('../../models/tenant/roleSchema');
 
-module.exports = async function (page, limit, dbName) {
+module.exports = async function (queryData, dbName) {
+    const { page, limit, q, type } = queryData;
     const skip = (page - 1) * limit;
     const User = getTenantModel(dbName, 'User', userSchema);
     const Role = getTenantModel(dbName, 'Role', roleSchema);
-    const totalUsers = await User.countDocuments();
-    const users = await User.find({}, { _id: 1, firstName: 1, lastName: 1, email: 1, role: 1, status: 1, lastLogin: 1, lastActivateAt: 1 })
+
+    const searchFilter = q
+        ? {
+            $or: [
+                {
+                    firstName: {
+                        $regex: q,
+                        $options: 'i'
+                    }
+                },
+                {
+                    lastName: {
+                        $regex: q,
+                        $options: 'i'
+                    }
+                },
+                {
+                    email: {
+                        $regex: q,
+                        $options: 'i'
+                    }
+                }
+            ]
+        } : {};
+
+    if (type) {
+        searchFilter.role = type
+    }
+    const totalUsers = await User.countDocuments(searchFilter);
+    const users = await User.find(searchFilter, { _id: 1, firstName: 1, lastName: 1, email: 1, role: 1, status: 1, lastLogin: 1, lastActivateAt: 1 })
         .populate('role', 'name')
         .skip(skip)
         .limit(limit)
