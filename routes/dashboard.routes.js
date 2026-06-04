45cc7a037366d2a846a5e3bce00f86a7ad76be62
Aman <ar3698892@gmail.com>
2026-06-04 18:35:11 +0530

    fixed bugs in role fetch api and separate dashboard api's

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -6,9 +6,18 @@ const apiResponse = require('../utils/apiResponse');
 const router = express.Router();
 
 router.get('/', verifyToken, asyncHandler(async function _getRecentUploadedDocs(req, res, next) {
-    const { docs, storageDetails, planDetails, docsAddedThisWeek } = await require('../controllers/dashboard/getDashBoardDetails')(req.tenant);
+    const tenant = req.tenant;
+    const { docs, storageDetails, planDetails, docsAddedThisWeek } = await require('../controllers/dashboard/getDashBoardDetails')(tenant);
     const stats = { storageDetails, planDetails, docsAddedThisWeek };
     return res.status(200).json(new apiResponse({ docs, stats }, 200, 'Dashboard data fetched succesfully'));
+}));
+
+router.get('/docs', verifyToken, authorize('view_document'), asyncHandler(async function _recentDocs(req, res, next) {
+    const limit = req.query.limit;
+    const tenant = req.tenant;
+    const docs = await require('../controllers/dashboard/recentDocs')(limit, tenant);
+    return res.status(200).json(new apiResponse({ docs }, 200, 'Recent Docs data fetched succesfully'));
+
 }));
 
 module.exports = router;
