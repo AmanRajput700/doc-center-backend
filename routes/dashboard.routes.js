b21b9ac3b3c1b265c808aa7f811656e7ef11c641
Aman <ar3698892@gmail.com>
2026-06-05 11:34:38 +0530

    Confirue Centralized Error logger

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -5,11 +5,11 @@ const asyncHandler = require('../utils/asyncHandler');
 const apiResponse = require('../utils/apiResponse');
 const router = express.Router();
 
-router.get('/', verifyToken, asyncHandler(async function _getRecentUploadedDocs(req, res, next) {
+router.get('/stats', verifyToken, asyncHandler(async function _getRecentUploadedDocs(req, res, next) {
     const tenant = req.tenant;
-    const { docs, storageDetails, planDetails, docsAddedThisWeek } = await require('../controllers/dashboard/getDashBoardDetails')(tenant);
+    const { storageDetails, planDetails, docsAddedThisWeek } = await require('../controllers/dashboard/getDashBoardDetails')(tenant);
     const stats = { storageDetails, planDetails, docsAddedThisWeek };
-    return res.status(200).json(new apiResponse({ docs, stats }, 200, 'Dashboard data fetched succesfully'));
+    return res.status(200).json(new apiResponse({ stats }, 200, 'Dashboard data fetched succesfully'));
 }));
 
 router.get('/docs', verifyToken, authorize('view_document'), asyncHandler(async function _recentDocs(req, res, next) {
