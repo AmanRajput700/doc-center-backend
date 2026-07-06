b12199e51bf281da6c39c62a1710b6e64c242e49
Aman <ar3698892@gmail.com>
2026-07-06 12:52:13 +0530

    Refactored code with the adding transaction and event with proper constant naming for the delete docs

--- controllers/document/deleteDocumentFromBin.js
+++ controllers/document/deleteDocumentFromBin.js
@@ -1,45 +1,60 @@
 const createHttpError = require('http-errors');
+
 const documentSchema = require('../../models/tenant/documentSchema');
-const { deleteObject } = require('../../services/s3.service');
-const getTenantModel = require('../../utils/getTenantModel');
-const { STATUS_CODE, ERROR_MESSAGE } = require('../../utils/constant');
 const storageSchema = require('../../models/tenant/storageSchema');
+
+const getTenantModel = require('../../utils/getTenantModel');
+const withTransaction = require('../../utils/withTransaction');
+
+const { deleteObject } = require('../../services/s3.service');
 const { updateApiAnalytics } = require('../../services/analyticsService');
+const { emitToTenant } = require('../../socket/services/emitService');
+
+const { STATUS_CODE, ERROR_MESSAGE } = require('../../utils/constant');
+const { DOCUMENT_DELETED } = require('../../socket/constants/events');
 
 module.exports = async function (tenant, docId) {
+
     const { dbName, _id: tenantId } = tenant;
 
     const Document = getTenantModel(dbName, 'Document', documentSchema);
+
     const Storage = getTenantModel(dbName, 'Storage', storageSchema);
 
     const doc = await Document.findOne({ _id: docId, isDeleted: true });
-    if (!doc) throw new createHttpError(STATUS_CODE.NOT_FOUND, ERROR_MESSAGE.DOC_NOT_FOUND);
+
+    if (!doc)
+        throw new createHttpError(STATUS_CODE.NOT_FOUND, ERROR_MESSAGE.DOC_NOT_FOUND);
 
 
     try {
         await deleteObject(doc.s3Key);
-        await Document.findByIdAndDelete(docId);
     } catch (error) {
-        throw new createHttpError(STATUS_CODE.INTERNAL_SERVER_ERROR, 'Unable to permanently delete document');
+        throw new createHttpError(STATUS_CODE.INTERNAL_SERVER_ERROR, 'Unable to permanently delete document.');
     }
 
-    const storage = await Storage.findOneAndUpdate(
-        {
-            tenantId
-        },
-        {
-            $inc: {
-                storageUsed: -doc.size,
-                trashedFiles: -1
+    // MongoDB transaction
+    const storage = await withTransaction(async (session) => {
+        await Document.findByIdAndDelete(doc._id, { session });
+        return await Storage.findOneAndUpdate(
+            {
+                tenantId
             },
-            $set: {
-                lastStorageUpdatedAt: new Date()
+            {
+                $inc: {
+                    storageUsed: -doc.size,
+                    trashedFiles: -1
+                },
+                $set: {
+                    lastStorageUpdatedAt: new Date()
+                }
+            },
+            {
+                new: true,
+                session
             }
-        },
-        {
-            new: true
-        }
-    ).lean();
+        ).lean();
+    });
 
     await updateApiAnalytics({
         dbName,
@@ -48,5 +63,7 @@ module.exports = async function (tenant, docId) {
         }
     });
 
+    emitToTenant(tenantId, DOCUMENT_DELETED);
+
     return doc;
-}
+};
--- socket/constants/events.js
+++ socket/constants/events.js
@@ -3,6 +3,13 @@ module.exports = {
     NOTIFICATION_READ: 'notification:read',
     NOTIFICATION_READ_ALL: 'notification:read-all',
     NOTIFICATION_UNREAD_COUNT: 'notification:unread-count',
-    DOCUMENT_UPLOADED: 'document-uploaded',
-    ROLE_CHANGED:'role-updated'
+    DOCUMENT_TRASHED: 'document:trashed',
+    DOCUMENT_UPLOADED: 'document:uploaded',
+    DOCUMENT_DELETED: 'document:deleted',
+    DOCUMENT_RESTORED: 'document:restored',
+    FOLDER_DELETED: 'folder:deleted',
+    FOLDER_TRASHED: 'folder:trashed',
+    FOLDER_RESTORED: 'folder:restored',
+    FOLDER_CREATED: 'folder:created',
+    ROLE_CHANGED: 'role:updated'
 };
